d03cdc4c2f3621c0ce7ecddef7cbe48c7b3455a4
GPiorek <GrzegorzPiorekkk@outlook.com>
2026-01-01 20:47:04 +0100

    added chaquopy config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk{
-            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            abiFilters += listOf("arm64-v8a", "x86_64")
+        }
+    }
+
+    chaquopy{
+        defaultConfig{
+            version= "3.14"
+            buildPython("")
+
         }
     }
 
